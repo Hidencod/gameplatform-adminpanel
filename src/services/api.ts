1b2e1c7e57097f8a4f25c614b6e2aace4b861179
devbouncinghead-gaming <jammanna017@gmail.com>
2026-04-08 08:49:26 +0530

    changed hard coded  url to .env url

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,7 +4,7 @@ import { logout } from "./authService";
 let isLoggingOut = false;
 export const api = axios.create(
     {
-        baseURL:import.meta.env.VITE_API_BASE_URL || "http://localhost:8080",
+        baseURL:import.meta.env.VITE_API_URL || "http://localhost:8080",
     }
 )
 api.interceptors.request.use((config) => {
